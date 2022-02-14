20f86d1e5b0d558abd521677db0152e450a00352
KostinaIrina <95071479+KostinaIrina@users.noreply.github.com>
2022-02-14 21:12:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// let
+// объявление переменной
+let number = 16; 
+// что бы изменить значение переменной не надо опять писать let. Оно пишется 1 раз.
+number = 20;
+console.log(number); 
+// const. используем эту переменную всегда только в том случае, когда мы точно знаем, что ее значение не будет изменяться никогда.
+const number2 = 100;
+console.log(number2);
+// арифметика
+const number3 = 100;
+const number4 = 10;
+
+let summary = number3 + number4;
+console.log(summary);
+
+// конкатенация строк. сложение нескольких сток, значений.
+
+const firstName = 'Ira';
+      lastName = 'Kostina';
+      
+let result = firstName + lastName;
+console.log(result); 
